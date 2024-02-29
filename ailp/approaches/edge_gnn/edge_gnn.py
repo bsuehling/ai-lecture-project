ffe3a01f2afe9faae3d94862a3f2f907a0c356cb
Benjamin Sühling <benjamin.suehling@t-online.de>
2024-02-29 18:33:08 +0100

    feat: work on edge_gnn model

--- ailp/approaches/edge_gnn/edge_gnn.py
+++ ailp/approaches/edge_gnn/edge_gnn.py
@@ -1,11 +1,12 @@
 import json
 import os
+import time
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
+from ailp.approaches.edge_gnn.model import EdgePredictor, NodeEncoder
 from ailp.approaches.prediction_model import PredictionModel
 from ailp.graph import Graph, Part
 
@@ -27,7 +28,10 @@ class EdgeGnnModel(PredictionModel):
         self._num_part_ids = len(self._part_map)
         self._num_family_ids = len(self._family_map)
 
-        self._model = nn.Module()
+        self._encoder = NodeEncoder(self._num_part_ids, self._num_family_ids)
+        self._predictor = EdgePredictor()
+
+        self._optimizer = torch.optim.Adam(self._encoder.parameters(), lr=0.01)
 
         self._n_epochs = n_epochs
 
@@ -45,8 +49,32 @@ class EdgeGnnModel(PredictionModel):
             for graph in eval_graphs
         ]
 
-        print(train_features)
-        print(eval_features)
+        for epoch in range(1, 1 + self._n_epochs):
+            start_time = time.perf_counter()
+            loss = self._train(train_features, train_graphs)
+            train_acc, test_acc = self._eval(eval_features, eval_graphs)
+            end_time = time.perf_counter() - start_time
+            print(
+                f"Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f} "
+                f"Eval: {test_acc:.4f}, Time: {end_time:.4f}"
+            )
+
+    def _train(self, graph_features: list[tuple[Tensor, Tensor]], graphs: list[Graph]):
+        self._encoder.train()
+        self._optimizer.zero_grad()
+        loss = 0
+        for features in graph_features:
+            encoded: Tensor = self._encoder(features, [])
+            mask = torch.tensor(encoded.shape[0])
+            edges = []
+            while not mask.all():
+                pred = self._predictor(encoded, mask, edges)
+                loss += F.nll_loss(pred, torch.tensor())  # TODO
+        loss.backward()
+        self._optimizer.step()
+
+    def _eval(self, features: list[Graph], graphs: list[Graph]):
+        0.0, 0.0
 
     def _prepare_node_features(self, parts: set[Part]) -> tuple[Tensor, Tensor]:
         part_ids = [self._part_map[str(part.part_id)] for part in parts]
--- ailp/approaches/edge_gnn/model.py
+++ ailp/approaches/edge_gnn/model.py
@@ -1,47 +1,42 @@
-from itertools import permutations
+from itertools import product
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.nn as gnn
 from torch import Tensor
-from torch_geometric.nn import RGCNConv
 
 
-class Model(nn.Module):
+class NodeEncoder(nn.Module):
     def __init__(self, n_part_ids: int, n_family_ids: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._forward_part_id = nn.Sequential(
-            nn.Linear(n_part_ids, 100),
-            nn.ReLU(),
-            nn.Linear(100, 10),
-            nn.ReLU(),
-            nn.Linear(10, 1),
-            nn.ReLU(),
+        self._encode_part_id = nn.Sequential(
+            nn.Linear(n_part_ids, 100), nn.ReLU(), nn.Linear(100, 8), nn.ReLU()
         )
-        self._forward_family_id = nn.Sequential(
-            nn.Linear(n_family_ids, 10), nn.ReLU(), nn.Linear(10, 1), nn.ReLU()
+        self._encode_family_id = nn.Sequential(
+            nn.Linear(n_family_ids, 10), nn.ReLU(), nn.Linear(50, 8), nn.ReLU()
         )
 
-        self._conv1 = RGCNConv(2, 8)
-        self._conv2 = RGCNConv(8, 16)
+    def forward(self, x: tuple[Tensor, Tensor]) -> Tensor:
+        x_part_id = self._encode_part_id(x[0])
+        x_family_id = self._encode_family_id(x[1])
+        out = torch.cat([x_part_id, x_family_id], dim=-1)
+        return out
 
-    def forward(self, x: tuple[Tensor, Tensor], edges: list[tuple[int, int]]):
-        x_part_id = self._forward_part_id(x[0])
-        x_family_id = self._forward_family_id(x[1])
 
-        n_nodes = len(x[0].shape[0])
-        perms = list(permutations(range(n_nodes), 2))
-        edge_index = torch.tensor(perms).T
-        edge_type = torch.zeros(n_nodes * (n_nodes - 1))
-        edge_mask = [
-            k for k, (i, j) in enumerate(perms) if (i, j) in edges or (j, i) in edges
-        ]
-        edge_type[edge_mask] = 1
+class EdgePredictor(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._conv1 = gnn.GraphConv(2, 8)
+        self._conv2 = gnn.GraphConv(8, 16)
+        self._fc_unconnected = nn.Sequential(nn.Linear(8, 16))
+        self._fc = nn.Sequential(nn.Linear(32, 64), nn.ReLU(), nn.Linear(64, 1))
 
-        x = torch.cat([x_part_id, x_family_id], dim=-1)
-
-        x = nn.ReLU(self._conv1(x, edge_index, edge_type=edge_type))
-        x = nn.ReLU(self._conv2(x, edge_index, edge_type=edge_type))
-
-        # TODO: for each edge, combine the feature of the 2 neighboring nodes
-
-        return x
+    def forward(self, x: Tensor, mask: Tensor, edges: Tensor) -> Tensor:
+        x_unconnected = self._fc_unconnected(x[~mask])
+        x_graph = F.relu(self._conv1(x[mask], edges))
+        x_graph = F.relu(self._conv2(x_graph, edges))
+        ec = torch.tensor(list(product(mask.tolist(), (~mask).tolist())))
+        edge_candidates = torch.cat((x_graph[ec[:, 0]], x_unconnected[ec[:, 1]]))
+        out = F.softmax(torch.squeeze(self._fc(edge_candidates)))
+        return out
